cb905848c59652d7af1b025f800f174ee4e1a109
Daniel Brazel <dbrazel4@gmail.com>
2017-12-31 01:22:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ function destructivelyAppendKitten(name){
 function destructivelyPrependKitten(name){
   kittens.unshift(name)
 }
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift
+}
+
+function appendKitten(name){
+  var newKittens = [kittens... name];
+  return newKittens
+}
